01c6bebb4cb1fe611d17b85a627ec81db1d3be62
Diego2207 <diego.santos@zup.com.br>
2019-06-25 16:19:42 -0300

    adicionar programa

---
+++ pagamentoComicao/src/com/br/comicao/Comicao.java
@@ -0,0 +1,28 @@
+package com.br.comicao;
+
+import java.util.Scanner;
+
+public class Comicao {
+	
+	
+	Scanner entrada = new Scanner(System.in);
+	
+	System.out.println("digite seu Id : ");
+	long idVendedor = entrada.nextLong();
+	
+	System.out.println("digite o código da peça : ");
+	int codPeça = entrada.nextInt();
+	
+	System.out.println("digite o preço da peça : ");
+	int preçoPeça = entrada.nextInt();
+	
+	System.out.println("informe quantas peças foram vendidas : ");
+	int peçasVendidas = entrada.nextInt();
+
+	double porcentagemPeça = (preçoPeça * 5)/100;
+	
+	System.out.println("a comissão do vendedor é de : " + porcentagemPeça);
+	
+}
+	Sysout
+}
